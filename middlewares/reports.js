d0db5c742f1c59644c15557e4431390567b4e8c0
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-03-18 22:28:50 +0900

    [ 이태현 ] 기능 수정: 신고 및 정지 미들웨어 사용자 ID 받는 방식 및 response 수정

--- middlewares/reports.js
+++ middlewares/reports.js
@@ -6,7 +6,7 @@ const User = require('../models/users');
 exports.userReport = async(req, res, next) => {
     // 테스트를 위해 req.user.id 로 User Id를 받았다.
     try {
-        user = await User.findOne({where: {id: req.user.id}});
+        user = await User.findOne({where: {id: req.userId}});
         user.reportCount = user.reportCount + 1;
 
         await user.save();
@@ -17,30 +17,40 @@ exports.userReport = async(req, res, next) => {
 
             await user.save();
 
-            console.log('After Suspended')
-            console.log(user)
-            console.log(user.reportCount)
-            console.log(user.suspensionCount)
-            console.log(user.type)
-            return res.status(200).json(
-                {
-                    code: 200,
-                    message: 'User_Type_Updated'
-                }
-            );
-        } else {
-            console.log('Before Suspended')
-            console.log(user)
-            console.log(user.reportCount)
-            console.log(user.suspensionCount)
-            console.log(user.type)
-            return res.status(200).json(
-                {
-                    code: 200,
-                    message: 'Suspension_Count_Updated'
-                }
-            );
-        }
+            // console.log('After Suspended')
+            // console.log(user)
+            // console.log(user.reportCount)
+            // console.log(user.suspensionCount)
+            // console.log(user.type)
+            // return res.status(200).json(
+            //     {
+            //         code: 200,
+            //         message: 'User_Type_Updated'
+            //     }
+            // );
+        } // else {
+            // console.log('Before Suspended')
+            // console.log(user)
+            // console.log(user.reportCount)
+            // console.log(user.suspensionCount)
+            // console.log(user.type)
+            // return res.status(200).json(
+            //     {
+            //         code: 200,
+            //         message: 'Suspension_Count_Updated'
+            //     }
+            // );
+        // }
+
+
+        // 게시글과 댓글 모두 사용자가 신고 처리 했을 때 위 과정을 거친 이후 아래 response를 받는다.
+        return res.status(200).json(
+            {
+                code: 200,
+                message: 'REPORT_SUCCESS'
+            }
+        )
+
     } catch (error) {
         console.log(error);
         return res.status(500).json(
