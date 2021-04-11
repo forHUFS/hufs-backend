14e0e0285243794a784a695794ff03a0eb65bfcb
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-04-12 00:43:08 +0900

    [ 이태현 ] 기능 수정: Session 구현 WIP

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ dotenv.config();
 
 const app          = express();
 const corsOptions = {
-    origin: "http://localhost:3000.com",
+    origin: "http://localhost:3000",
     credentials: true
 }
 
